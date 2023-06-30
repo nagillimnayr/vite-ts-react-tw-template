6138f768dc948e218277113a2c91e4ec177f157a
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-29 19:54:53 -0700

    build: install tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/index.css';
+
 const main = () => {
-  const rootElement = document.createElement('main');
-  document.body.appendChild(rootElement);
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+    const rootElement = document.createElement('main');
+    document.body.appendChild(rootElement);
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 };
 
 window.addEventListener('load', main);
